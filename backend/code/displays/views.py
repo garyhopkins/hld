0347543cfe8a1defd5e01f458f750824e2fd8e81
Gary Hopkins <gary@verbidio.com>
2022-12-12 14:51:16 -0600

    added URL routes

--- backend/code/displays/views.py
+++ backend/code/displays/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Function that is called from the urls.py file... can be named anything, doesn't have to be index
+def index(request):
+    response = "<H1><span style=""color:#84be3f"">This is the displays route. Display listings will show on this page.</span> <br><br><span style=""color:red"">Need template with a form to submit a new display.</span>"
+    return HttpResponse(response)
--- backend/code/mysite/urls.py
+++ backend/code/mysite/urls.py
@@ -14,8 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+# The include() function allows referencing other URLconfs. Whenever Django encounters include(), it chops off whatever part of the URL matched up to that point and sends the remaining string to the included URLconf for further processing.
+# The idea behind include() is to make it easy to plug-and-play URLs. Since displays are in their own URLconf (displays/urls.py), they can be placed under “/displays/”, or under “/fun_displays/”, or under “/content/displays/”, or any other path root, and the app will still work.
+# You should always use include() when you include other URL patterns. admin.site.urls is the only exception to this.
+
+
 
 urlpatterns = [
+    # The path() function is passed four arguments, two required: route and view, and two optional: kwargs, and name.
+    # path() argument: route - route is a string that contains a URL pattern. When processing a request, Django starts at the first pattern in urlpatterns and makes its way down the list, comparing the requested URL against each pattern until it finds one that matches. 
+    # Patterns don’t search GET and POST parameters, or the domain name.
+    # path() argument: view - When Django finds a matching pattern, it calls the specified view function with an HttpRequest object as the first argument and any “captured” values from the route as keyword arguments. 
+    # path() argument: kwargs - (optional) Arbitrary keyword arguments can be passed in a dictionary to the target view.
+    # path() argument: name - (optional) Naming your URL lets you refer to it unambiguously from elsewhere in Django, especially from within templates. This powerful feature allows you to make global changes to the URL patterns of your project while only touching a single file.
+
+    # Points the root URLconf at the displays.urls module. In displays/urls.py, add an import for django.urls.include and insert an include() in the urlpatterns list.
+    path('displays/', include('displays.urls')),
+    # TODO: Add redirect for '' path
+    # path('', include('displays.urls')),
+    # Points to the default Admin page.
     path("admin/", admin.site.urls),
 ]
